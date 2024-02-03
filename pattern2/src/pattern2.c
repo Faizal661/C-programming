26d1f9fd7a853032764eda2ef6b452f2c60b51fe
faizal <faisalt661@gmail.com>
2024-02-03 14:35:16 -0500

    commit

--- pattern2/src/pattern2.c
+++ pattern2/src/pattern2.c
@@ -13,37 +13,38 @@
 
 int main(void) {
 	int i,j,k,n,space;
-	//scanf("%d",&n);
-	n=7;
+	setbuf(stdout,NULL);
+	printf("Enter number of lines :");
+	scanf("%d",&n);
 	space=(n*2)-2;
 
 	for(i=1;i<=n;i++){
 		for(j=0;j<i;j++){
-		    printf("*");
+			printf("*");
 		}
 		for(k=1;k<=space;k++){
 			printf(" ");
 		}
 		for(j=0;j<i;j++){
-		    printf("*");
+			printf("*");
 		}
 		printf("\n");
 		space=space-2;
 	}
 	space=2;
 
-	for(i=1;i<n;i++){
-			for(j=i;j<n;j++){
-			    printf("*");
-			}
-			for(k=1;k<=space;k++){
-				printf(" ");
-			}
-			for(j=i;j<n;j++){
-			    printf("*");
-			}
-			printf("\n");
-			space=space+2;
+	for(i=1;i<=n;i++){
+		for(j=i;j<n;j++){
+			printf("*");
 		}
+		for(k=1;k<=space;k++){
+			printf(" ");
+		}
+		for(j=i;j<n;j++){
+			printf("*");
+		}
+		printf("\n");
+		space=space+2;
+	}
 	return EXIT_SUCCESS;
 }
